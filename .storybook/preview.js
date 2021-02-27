8cf9cfdfb24e3e35d5f09d1f452ba40debf05953
David <davidvargas.d45@gmail.com>
2021-02-27 17:02:37 -0500

    documentation: storybook setup implemented

---
+++ .storybook/preview.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const getStyles = ({ __sb } = {}) => ({
+  display: 'flex',
+  flexDirection: __sb?.fd || 'column',
+  maxHeight: __sb?.mh || 'auto',
+  justifyContent: 'flex-start',
+  alignContent: 'flex-start',
+  flexWrap: 'wrap',
+  height: '100%',
+  gap: '10px 30px',
+});
+
+export const decorators = [
+  (Story, { parameters }) => (
+    <div style={getStyles(parameters)}>
+      <Story />
+    </div>
+  ),
+];
+
+export const parameters = {
+  argTypes: {
+    styles: { table: { disable: true } },
+    getStyles: { table: { disable: true } },
+  },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
