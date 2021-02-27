390ea422ad536380b4b84884d6912a4b8ecc52f6
David <davidvargas.d45@gmail.com>
2021-02-27 17:54:34 -0500

    feat: input component base added

---
+++ components/atoms/Input/Input.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Input.module.css';
+import { options } from './constants';
+import withStyles from 'hocs/withStyles';
+
+export const Input = ({
+  getStyles,
+  type,
+  value,
+  isInline,
+  onChange,
+  placeholder,
+}) => (
+  <input
+    className={getStyles('input', {
+      'is-inline': isInline,
+    })}
+    type={type}
+    value={value}
+    placeholder={placeholder}
+    onChange={onChange}
+  ></input>
+);
+
+Input.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  getStyles: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(options.types),
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  isInline: PropTypes.bool,
+};
+
+Input.defaultProps = {
+  type: 'text',
+  value: '',
+  placeholder: '',
+  isInline: false,
+  onChange: () => {},
+  getStyles: () => {},
+};
+
+export default withStyles(styles)(Input);
