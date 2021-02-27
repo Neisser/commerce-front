be72b2674493c68853a0a83f840c8375519b21e9
David <davidvargas.d45@gmail.com>
2021-02-27 17:33:24 -0500

    feat: button component base added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/styles/styles.module.css';
 
 const getStyles = ({ __sb } = {}) => ({
   display: 'flex',
---
+++ components/atoms/Button/Button.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Heading from 'atoms/Heading';
+
+import styles from './Button.module.css';
+import { options } from './constants';
+import withStyles from 'hocs/withStyles';
+
+export const Button = ({
+  type,
+  children,
+  addons,
+  isMuted,
+  isInline,
+  onClick,
+  getStyles,
+}) => (
+  <button
+    className={getStyles('button', ['type'], {
+      'is-inline': isInline || type === 'tertiary',
+      'is-muted': isMuted && type === 'primary',
+    })}
+    onClick={onClick}
+  >
+    {addons && addons.prepend}
+    <Heading color={type === 'primary' ? 'inverted' : 'primary'}>
+      {children}
+    </Heading>
+    {addons && addons.append}
+  </button>
+);
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  getStyles: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(options.types),
+  addons: PropTypes.shape({
+    prepend: PropTypes.node,
+    append: PropTypes.node,
+  }),
+  onClick: PropTypes.func,
+  isInline: PropTypes.bool,
+  isMuted: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  type: 'primary',
+  getStyles: () => {},
+  onClick: () => {},
+  isInline: false,
+};
+
+export default withStyles(styles)(Button);
