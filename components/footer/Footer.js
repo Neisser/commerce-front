82eccc12022b73b452ec4322966572d284b150d4
William Galvis <william.galvis@pluriza.com>
2021-02-28 22:14:12 -0500

    refactor(footer): change footer class.

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,61 +1,55 @@
 import React from 'react';
 import styles from './footer.module.css';
-import PropTypes from "prop-types";
-import { options } from './constants';
-import withStyles from "../../hocs/withStyles";
+import PropTypes from 'prop-types';
+import withStyles from '../../hocs/withStyles';
 import Icon from '@material-ui/core/Icon';
 
-const ROOT = '/';
-const routes = [
-    {
-        route: ROOT,
-        label: 'home'
-    },
-    {
-        route: '/home-section',
-        label: 'home-section'
-    }
-];
+import Paragraph from 'atoms/Paragraph';
+export const Footer = ({ getStyles }) => {
+  return (
+    <footer className={getStyles('footer', 'mt-10')}>
+      <div className={getStyles('div', 'px-20')}>
+        {/* Logo */}
+        <div className={getStyles('logo')}>
+          <Paragraph className={'text-white'} size={'2xl'} weigth={'bold'}>
+            Logo
+          </Paragraph>
+        </div>
 
-export const Footer = ({getStyles}) => {
-    return (
-        <footer className={getStyles('footer', 'size-sm', 'size-md', 'size-lg')}>
-            <div className={getStyles('div')}>
+        <div className={getStyles('text', 'mt-4')}>
+          <Paragraph className={'text-white'} size={'lg'}>
+            clusthers@gmail.com
+          </Paragraph>
+        </div>
 
-                {/* Logo */}
-                <div className={getStyles('logo')}>
-                    <img className="" alt="logo"/> 
-                </div>
+        <div className={getStyles('icon')}>
+          <Icon>fiber_manual_record</Icon>
+          <Icon>fiber_manual_record</Icon>
+          <Icon>fiber_manual_record</Icon>
+        </div>
 
-                {/* Contenido */}
-                <div className={getStyles('text','size-sm', 'size-md', 'size-lg')}>
-                    <p>
-                    FAQ<br/>
-                    Shipping information <br/>
-                    Privacy policy <br/>
-                    Terms and conditions
-                    </p>
-                </div>
-
-                {/* Iconos */}
-                <div className={getStyles('icon')}>
-                    <Icon>mail</Icon>
-                    <Icon>mail</Icon>
-                    <Icon>mail</Icon>
-                </div>
-
-                
-            </div>
-               
-      
-        </footer>
+        {/* Contenido */}
+        <div className={getStyles('text', 'mt-4')}>
+          <Paragraph className={'text-white'} size={'lg'}>
+            FAQ
+          </Paragraph>
+          <Paragraph className={'text-white'} size={'lg'}>
+            Shipping information
+          </Paragraph>
+          <Paragraph className={'text-white'} size={'lg'}>
+            Privacy policy
+          </Paragraph>
+          <Paragraph className={'text-white'} size={'lg'}>
+            Terms and conditions
+          </Paragraph>
+        </div>
         
-
-    )
-}
+      </div>
+    </footer>
+  );
+};
 Footer.prototype = {
-    getStyles: PropTypes.func.isRequired,
-  };
-  
-export default withStyles(styles)(Footer);
+  getStyles: PropTypes.func.isRequired,
+};
 
+export default withStyles(styles)(Footer);
