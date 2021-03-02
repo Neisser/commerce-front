35859af798c9ec1b29de666a50822c4ce9bb6459
David <davidvargas.d45@gmail.com>
2021-03-01 22:31:19 -0500

    fix(Navbar): Navbar module css name file fixed, navbar -> Navbar

--- components/organisms/Navbar/Navbar.js
+++ components/organisms/Navbar/Navbar.js
@@ -1,21 +1,19 @@
-import { useCallback, useState } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
+import { useCallback, useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 import Icon from '@material-ui/core/Icon';
 import Badge from '@material-ui/core/Badge';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-import withStyles from "../../../hocs/withStyles";
-import styles from "./Navbar.module.css";
+import withStyles from '../../../hocs/withStyles';
+import styles from './Navbar.module.css';
 
 import Button from 'atoms/Button';
 
 import Modal from 'molecules/Modal';
 import { routes } from 'helpers/constants';
 
-import Menu from 'organism/Menu'
-
-
+import Menu from '@/organisms/Menu';
 
 export const Navbar = ({ getStyles }) => {
   // console.log(props);
@@ -26,8 +24,8 @@ export const Navbar = ({ getStyles }) => {
   //     onChange(value)
   // }, []);
 
-  const [ showMenu, setShowMenu ] = useState(false);
-  const [ showModal, setShowModal ] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   // Example to tranlate page
   // /**
@@ -45,19 +43,16 @@ export const Navbar = ({ getStyles }) => {
     return (
       <div className="sm:show" id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1">
-          {
-            routes.map((r) => (
-              <Link href={r.route}>
-                <a className={getStyles("list-option")}>{r.label}</a>
-              </Link>
-            ))
-          }
+          {routes.map((r) => (
+            <Link href={r.route}>
+              <a className={getStyles('list-option')}>{r.label}</a>
+            </Link>
+          ))}
         </div>
       </div>
-    )
-  }
+    );
+  };
 
-  
   return (
     <>
       {/* 
@@ -76,16 +71,32 @@ export const Navbar = ({ getStyles }) => {
       <nav className={getStyles('navbar', 'sticky top-0')}>
         <div className={getStyles('navbar-wrapper')}>
           <div className={getStyles('navbar-align-items')}>
-            <div onClick={() => setShowMenu(!showMenu)} className={getStyles('navbar-icon-menu-wrapper','sm:show')}>
+            <div
+              onClick={() => setShowMenu(!showMenu)}
+              className={getStyles('navbar-icon-menu-wrapper', 'sm:show')}
+            >
               <Icon>menu</Icon>
             </div>
-            <div className={getStyles()} className="lg:items-stretch lg:justify-start mx-10">
+            <div
+              className={getStyles()}
+              className="lg:items-stretch lg:justify-start mx-10"
+            >
               LOGO
             </div>
-            <div className={getStyles('navbar-icons-right-wrapper', 'sm:static sm:inset-auto sm:ml-6 sm:pr-5')}>
+            <div
+              className={getStyles(
+                'navbar-icons-right-wrapper',
+                'sm:static sm:inset-auto sm:ml-6 sm:pr-5'
+              )}
+            >
               <Icon>search</Icon>
               <Badge badgeContent={3} color="error">
-                <Icon onClick={() => setShowModal(!showModal)} className="cursor-pointer">shopping_cart</Icon>
+                <Icon
+                  onClick={() => setShowModal(!showModal)}
+                  className="cursor-pointer"
+                >
+                  shopping_cart
+                </Icon>
               </Badge>
               <Icon>person</Icon>
             </div>
@@ -93,23 +104,19 @@ export const Navbar = ({ getStyles }) => {
         </div>
       </nav>
 
-      {showModal ? <Modal showModal={showModal} setShowModal={setShowModal}>
-        Some Content
-        <hr />
-        <br />
-        <div className="flex flex-col space-y-1 px-6">
-          <Button
-            type="primary"
-          >
-            Pagar
-          </Button>
-          <Button
-            type="primary"
-          >
-            Ver Carrito
-          </Button>
-        </div>
-      </Modal> : false}
+      {showModal ? (
+        <Modal showModal={showModal} setShowModal={setShowModal}>
+          Some Content
+          <hr />
+          <br />
+          <div className="flex flex-col space-y-1 px-6">
+            <Button type="primary">Pagar</Button>
+            <Button type="primary">Ver Carrito</Button>
+          </div>
+        </Modal>
+      ) : (
+        false
+      )}
     </>
   );
 };
